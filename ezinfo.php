3100e2e0670c6dd4f09b124fd4dfec3734709674
Edi Modrić <edi@netgen.hr>
2013-06-12 12:02:31 +0000

    Bump version

--- ezinfo.php
+++ ezinfo.php
@@ -7,7 +7,7 @@ class ngpushInfo
         $eZCopyrightString = 'Copyright (C) 2010-2013 Netgen d.o.o.';
 
         return array( 'Name'      => '<a href="http://projects.ez.no/ngpush">Netgen Push</a> extension',
-                      'Version'   => '1.2',
+                      'Version'   => '1.3',
                       'Copyright' => $eZCopyrightString,
                       'License'   => 'GNU General Public License v2.0'
                     );
